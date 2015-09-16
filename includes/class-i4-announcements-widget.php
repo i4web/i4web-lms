c78b020fda85cef057ed44e9ce3d705f28735e9e
jrivera <jrivera@picernefl.com>
2015-09-16 09:15:02 -0400

    Tweaks
    Tweak the announcement widget to test the design.

--- includes/class-i4-announcements-widget.php
+++ includes/class-i4-announcements-widget.php
@@ -29,5 +29,53 @@
     /** @see WP_Widget::widget */
     function widget( $args, $instance ) {
 
+      $args['id']        = ( isset( $args['id'] ) ) ? $args['id'] : 'I4Web_LMS_Announcements_Widget';
+      $instance['title'] = ( isset( $instance['title'] ) ) ? $instance['title'] : '';
+      $title = $instance['title'];
+
+      echo $args['before_widget'];
+
+      if( $title ){
+        echo $args['before_title'] . $title . $args['after_title'];
+      }
+
+      echo '<ul class="announcements-list no-bullet">
+              <li><span class="announcement-feat-img"><img src="https://www.floridahospital.com/sites/default/files/styles/what-is-happening/public/little_magic_baby_onesie_sm.jpg?itok=T9yBG6L7"></span><a href="#">A Culture of Caffeine</a>
+              <p class="announcement-posted-date">November 1, 2015</p>
+              </li>
+              <li><span class="announcement-feat-img"><img src="https://www.floridahospital.com/sites/default/files/styles/what-is-happening/public/body-weight-workout.jpg?itok=kEvxu4Pt"></span><a href="#">10 Minute Workout to Jumpstart Your Day</a>
+              <p class="announcement-posted-date">November 1, 2015</p>
+              </li>
+            </ul>';
+      echo '<a href="#" class="button expand">Read All</a>';
+
+      echo $args['after_widget'];
+
+    }
+
+    /** @see WP_Widget::update */
+  	function update( $new_instance, $old_instance ) {
+  		$instance = $old_instance;
+
+  		$instance['title']            = strip_tags( $new_instance['title'] );
+  		$instance['hide_on_checkout'] = isset( $new_instance['hide_on_checkout'] );
+
+  		return $instance;
+  	}
+
+    /** @see WP_Widget::form */
+    function form ( $instance ){
+
+      $defaults = array(
+  			'title'            => 'Announcements'
+  		);
+
+      $instance = wp_parse_args( (array) $instance, $defaults ); ?>
+      <p>
+  			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Title:', 'i4web' ); ?></label>
+  			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo $instance['title']; ?>"/>
+  		</p>
+
+      <?php
     }
   }
