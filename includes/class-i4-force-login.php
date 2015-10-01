251a268407ab9738112f50ee00b343d9f38af030
jrivera <jrivera@i-4web.com>
2015-10-01 00:16:49 -0400

    Fix AJAX bug for certain user roles
    Added a check for the DOING_AJAX constant when restricting the admin
    area.

--- includes/class-i4-force-login.php
+++ includes/class-i4-force-login.php
@@ -27,7 +27,7 @@
      public function __construct(){
 
        //Insert the Login Check into the wp_head of the website
-       add_action('wp_head', array($this, 'i4_force_login') );
+       add_action('wp_head', array($this, 'i4_login_redirect') );
 
        //Restrict the Admin area
        add_action( 'admin_init', array($this, 'i4_restrict_admin' ) );
@@ -39,7 +39,7 @@
      * the page trying to be accessed after the user has logged in
      *
      */
-     public function i4_force_login(){
+     public function i4_login_redirect(){
 
        if( !is_user_logged_in() ){ //Redirect the user if they are not authenticated
 		    auth_redirect();
@@ -53,7 +53,7 @@
       */
       public function i4_restrict_admin(){
 
-        if ( ! current_user_can( 'edit_pages' ) ) {
+        if ( ! current_user_can( 'edit_pages' ) && ! ( defined( 'DOING_AJAX' ) && DOING_AJAX )) {
             wp_safe_redirect( site_url() );
             exit;
       	}
