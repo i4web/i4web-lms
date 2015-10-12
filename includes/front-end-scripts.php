9ba32c9c0525b1a0a59fc42480f32c0a791d26ed
jrivera <jrivera@i-4web.com>
2015-10-12 00:00:46 -0400

    test

--- includes/front-end-scripts.php
+++ includes/front-end-scripts.php
@@ -13,27 +13,27 @@
  */
 function i4_lms_scripts() {
 
-	//Load the scripts that we'll need to use for the theme
-	wp_enqueue_script( 'i4-ajax-front-end', I4_PLUGIN_URL .'assets/js/ajax-front-end.js', array('jquery', 'wpcw-jquery-form', 'wpcw-countdown'), '0.0.1', true);
-	wp_enqueue_script( 'vimeo-froogaloop', I4_PLUGIN_URL . 'assets/js/froogaloop.min.js', array( ), '2.0', false);
-	wp_enqueue_script( 'i4-main-js', I4_PLUGIN_URL . 'assets/js/main.js', array('jquery', 'vimeo-froogaloop'), '0.0.1', false);
-	wp_enqueue_script( 'password-strength-meter' );
-	wp_dequeue_script( 'wpcw-frontend');
+    //Load the scripts that we'll need to use for the theme
+    wp_enqueue_script( 'i4-ajax-front-end', I4_PLUGIN_URL .'assets/js/ajax-front-end.js', array('jquery', 'wpcw-jquery-form', 'wpcw-countdown'), '0.0.1', true);
+    wp_enqueue_script( 'vimeo-froogaloop', I4_PLUGIN_URL . 'assets/js/froogaloop.min.js', array( ), '2.0', false);
+    wp_enqueue_script( 'i4-main-js', I4_PLUGIN_URL . 'assets/js/main.js', array('jquery', 'vimeo-froogaloop'), '0.0.1', false);
+    wp_enqueue_script( 'password-strength-meter' );
+    wp_dequeue_script( 'wpcw-frontend');
 
-	// Variable declarations
+    // Variable declarations
 wp_localize_script(
-			'i4-ajax-front-end', 	// What we're attaching too
-			'wpcw_js_consts_fe',		// Handle for this code
+            'i4-ajax-front-end',     // What we're attaching too
+            'wpcw_js_consts_fe',        // Handle for this code
 array(
-		'ajaxurl'				=> admin_url('admin-ajax.php'), // URL for admin AJAX
-		'progress_nonce'		=> wp_create_nonce('wpcw-progress-nonce'), 	// Nonce security token
-		'str_uploading'			=> __('Uploading:', 'wp_courseware'),		// Uploading message.
-		'str_quiz_all_fields'	=> __('Please provide an answer for all of the questions on this page.', 'wp_courseware'),
+        'ajaxurl'                => admin_url('admin-ajax.php'), // URL for admin AJAX
+        'progress_nonce'        => wp_create_nonce('wpcw-progress-nonce'),     // Nonce security token
+        'str_uploading'            => __('Uploading:', 'wp_courseware'),        // Uploading message.
+        'str_quiz_all_fields'    => __('Please provide an answer for all of the questions on this page.', 'wp_courseware'),
 
-		// Timer units
-		'timer_units_hrs' 		=> __('hrs', 'wp_courseware'),
-		'timer_units_mins' 		=> __('mins', 'wp_courseware'),
-		'timer_units_secs' 		=> __('secs', 'wp_courseware'),
+        // Timer units
+        'timer_units_hrs'         => __('hrs', 'wp_courseware'),
+        'timer_units_mins'         => __('mins', 'wp_courseware'),
+        'timer_units_secs'         => __('secs', 'wp_courseware'),
 
 ));
 
